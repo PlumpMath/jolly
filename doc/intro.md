375c7ade21181daad6942c3346ec4af1e46c1313
Richard Möhn <richard.moehn@posteo.de>
2015-08-09 11:40:36 +0900

    Transform Any Bars before packaging
    Before this, the Grimoire→Grenada converter just dumped the Grimoire
    data in an Any Bar. This was not very helpful. Therefore use the new
    specify-cmeta-any from lib-grenada to split the Cmetadata up into nice
    meaningful Bars before putting them in the JAR.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jolly
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [me.arrdem/guten-tag "0.1.4"]
-                 [org.clj-grenada/lib-grenada "0.3.0"]
+                 [org.clj-grenada/lib-grenada "0.3.3-SNAPSHOT"]
+                 ;; Remove the (remove … ".git") when bumping.
                  [org.clojure-grimoire/lib-grimoire "0.10.2"]
                  [prismatic/plumbing "0.4.0"]
                  [prismatic/schema "0.4.3"]])
