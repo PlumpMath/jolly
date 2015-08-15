4c5c06a44ff123bd1749873b87791f1c4e953a94
Richard Möhn <richard.moehn@posteo.de>
2015-08-15 12:43:16 +0900

    Extract Bars from Cmetadata in the Any Bar
    Use a new feature of lib-grenada: if people provide a Bar in the
    :grenada.cmeta/bars entry in a concrete thing's Cmetadata, attach it as
    a full-fledged Bar to the Thing.

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "MIT license"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [dire "0.5.3"]
                  [me.arrdem/guten-tag "0.1.4"]
                  [org.clj-grenada/lib-grenada "0.3.3-SNAPSHOT"]
+                 [org.clj-grenada/poomoo "0.1.0-SNAPSHOT"]
                  ;; Remove the (remove … ".git") when bumping.
                  [org.clojure-grimoire/lib-grimoire "0.10.2"]
                  [prismatic/plumbing "0.4.0"]
