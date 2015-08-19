898d5dd9bd6412ae15ede4a3fb2f9986f93a89fd
Richard Möhn <richard.moehn@posteo.de>
2015-08-19 15:26:10 +0900

    Bump lib-grenada to 1.0.0-rc.2

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [dire "0.5.3"]
                  [org.clojars.rmoehn/guten-tag "0.1.5"]
-                 [org.clj-grenada/lib-grenada "1.0.0-rc.1"]
+                 [org.clj-grenada/lib-grenada "1.0.0-rc.2"]
                  [org.clj-grenada/poomoo "1.0.0-rc.1"]
                  [org.clojars.rmoehn/lib-grimoire "0.10.3"]
                  [prismatic/plumbing "0.4.0"]
